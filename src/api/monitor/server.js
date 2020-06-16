7d0bfffdf994be13a02d117d14fe726d1b7baff8
maplefix <wangjg@landicorp.com>
2020-06-16 17:12:41 +0800

    新增服务器状态信息数据查询api

---
+++ src/api/monitor/server.js
@@ -0,0 +1,12 @@
+import request from '@/utils/request'
+
+/**
+ * 查询服务器详细
+ * @returns {AxiosPromise}
+ */
+export function getServer() {
+  return request({
+    url: '/server',
+    method: 'get'
+  })
+}
