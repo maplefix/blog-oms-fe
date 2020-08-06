b5443a0b4f609fd9f801af3cad801a2ae848d7f8
maplefix <wangjg@landicorp.com>
2020-08-06 11:08:17 +0800

    新增登陆模块及主页

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="assets/logo/logo.png">
+    <!--<img alt="Vue logo" src="assets/logo/logo.png">-->
     <router-view/>
   </div>
 </template>
