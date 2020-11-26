6f5e072fc22f3d42c79f66e111f9a30879fbdd08
maplefix <wangjg@landicorp.com>
2020-11-26 10:57:58 +0800

    引入element-ui组件

---
+++ src/components/MapleBlog/Doc/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="skill" @click="goto"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MapleBlogDoc',
+    data() {
+      return {
+        url: 'https://www.maplefix.top'
+      }
+    },
+    methods: {
+      goto() {
+        window.open(this.url)
+      }
+    }
+  }
+</script>
---
+++ src/components/MapleBlog/Git/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MapleBlogGit',
+    data() {
+      return {
+        url: 'https://github.com/maplefix/maple-blog'
+      }
+    },
+    methods: {
+      goto() {
+        window.open(this.url)
+      }
+    }
+  }
+</script>
