0628bcd6b2f39b3572b88385ce55443d6e4142f2
maplefix <wangjg@landicorp.com>
2020-11-30 10:34:40 +0800

    存储模块新增

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
+
 import Cookies from 'js-cookie'
-import 'normalize.css/normalize.css' //a modern alternative to CSS resets
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
 import Element from 'element-ui'
 import './assets/styles/element-variables.scss'
 
-import '@/assets/styles/index.scss' //global css
-import '@/assets/styles/maple.scss' //custom css
+import '@/assets/styles/index.scss' // global css
+import '@/assets/styles/maple.scss' //
 import App from './App'
 import store from './store'
 import router from '@/router/routers'
-
-import './assets/icons' //icon
-import '@/router' //permission controlnpm audit
 import permission from './directive/permission'
 
 import './assets/icons' // icon
@@ -21,14 +21,14 @@ import {getConfigKey} from "@/api/system/config";
 import {parseTime, resetForm, addDateRange, selectDictLabel,handleTree} from "@/utils/maple";
 import Pagination from "@/components/Pagination";
 
-//全局方法挂载
+// 全局方法挂载
 Vue.prototype.getDicts = getDicts;
 Vue.prototype.getConfigKey = getConfigKey;
 Vue.prototype.parseTime = parseTime;
 Vue.prototype.resetForm = resetForm;
 Vue.prototype.addDateRange = addDateRange;
 Vue.prototype.selectDictLabel = selectDictLabel;
-Vue.prototype.handleTree = handleTree;
+Vue.prototype.handleTree = handleTree
 
 Vue.prototype.msgSuccess = function (msg) {
   this.$message({showClose: true, message: msg, type: "success"});
@@ -52,15 +52,23 @@ Vue.component('Pagination', Pagination);
 
 Vue.use(permission);
 
-Vue.use(Element,{
-  size: Cookies.get('size') || 'medium' //set element-ui default size
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online! ! !
+ */
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
 });
 
 Vue.config.productionTip = false;
 
-
 new Vue({
-   el: '#app',
+  el: '#app',
   router,
   store,
   render: h => h(App)
